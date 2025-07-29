0c4511418a2f91a8851ab9f7a8c0e2224cf43c13
Aniket Sharma <anikets.5061@gmail.com>
2025-07-29 22:52:43 +0530

    first commit

--- script.js
+++ script.js
@@ -3,4 +3,14 @@ function toggleMenu() {
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
+  const toggleBtn = document.getElementById("toggle-mode");
+  const body = document.body;
+
+  
 }
+
+const toggleBtn = document.getElementById("toggle-mode");
+    toggleBtn.addEventListener("click", () => {
+      document.body.classList.toggle("dark-mode");
+      toggleBtn.textContent = document.body.classList.contains("dark-mode") ? "☀️" : "🌙";
+    });
